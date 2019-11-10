f656a9f4d69fdddff5b2964361d6ab0f298705f2
durgaswaroop <durgaswaroop@gmail.com>
2019-11-10 21:04:46 +0530

    Add working code of conway's game of life

---
+++ game_of_life/life.py
@@ -0,0 +1,121 @@
+import argparse
+import itertools
+import os
+import sys
+import time
+
+from random import randint
+from copy import deepcopy
+
+# Setup cmd line options
+parser = argparse.ArgumentParser(description="Conway's game of life")
+parser.add_argument('--no-loop', 
+        action='store_true',
+        help='Whether to show only one state or keep looping')
+parser.add_argument('--size',
+        type=int,
+        default=35,
+        help='Number of rows')
+parser.add_argument('--configuration', 
+        default='random',
+        help='Specific configuration as a starting point')
+parser.add_argument('--interval',
+        default=0.02,
+        type=float,
+        help='Time interval between generations')
+
+class Universe:
+    def __init__(self, size = 35, configuration='random'):
+        self.rows = size
+        self.cols = size * 2
+        if configuration == 'random' :
+            self.state = self.random()
+        elif configuration == 'glider' :
+            self.state = self.glider()
+        else:
+            print(f'[ERROR]: Configuration {configuration} not supported')
+            return
+
+    def random(self):
+        """Generate random starting configuration."""
+        return [[randint(0,1) for _ in range(self.cols)] for _ in range(self.rows)]
+
+    def glider(self):
+        """Generate the configuration for a glider."""
+        temp = [[0 for _ in range(self.cols)] for _ in range(self.rows)]
+        temp[0][-3] = 1
+        temp[1][-3] = 1
+        temp[1][-1] = 1
+        temp[2][-3] = 1
+        temp[2][-2] = 1
+        return temp
+
+    def _neighbors(self, r, c):
+        """Get the neighbors of a cell at row r and column c."""
+        all_possible = [(r-1, c-1),
+                (r-1, c  ),
+                (r-1, c+1),
+                (r  , c-1),
+                (r  , c+1),
+                (r+1, c-1),
+                (r+1, c  ),
+                (r+1, c+1)
+                ]
+
+        actual = [(r,c) for (r,c) in all_possible \
+                if (r>=0 and c>=0 and \
+                r<self.rows and c<self.cols)]
+        return [self.state[r][c] for r,c in actual]
+
+    def element_next_state(self, r, c):
+        """Get the state of the element at row r and column c in the next iteration."""
+        alive = bool(self.state[r][c])
+        neighbors_around = sum(self._neighbors(r,c))
+        if alive:
+            if neighbors_around == 2 or neighbors_around == 3:
+                return 1
+            else:
+                return 0
+        else:
+            return 1 if neighbors_around == 3 else 0
+
+    def next(self):
+        """Get the next state of the universe."""
+        temp = deepcopy(self.state)
+        for i in range(self.rows):
+            for j in range(self.cols):
+                temp[i][j] = self.element_next_state(i,j)
+        self.state = temp
+        return self
+
+def render(universe):
+    """Print out the universe."""
+    def print_row(row):
+        for e in ['ı'] + row + ['ı']:
+            print(' ' if e==0 else ('■' if e==1 else 'ı'), end='') 
+        print()
+
+    print('-' * (universe.cols+2))
+    for row in universe.state:
+        print_row(row)
+    print('-' * (universe.cols+2))
+
+if __name__ == '__main__':
+    arguments = parser.parse_args(sys.argv[1:])
+    universe = Universe(arguments.size, arguments.configuration)
+
+    # Generation starts at zero
+    generation = 0
+
+    render(universe)
+    print(f'Generation: {generation}')
+
+    if not arguments.no_loop:
+        os.system('clear')
+        while True:
+            render(universe.next())
+            generation = generation + 1
+            print(f'Generation: {generation}')
+            time.sleep(arguments.interval)
+            os.system('clear')
+
---
+++ game_of_life/test_life.py
@@ -0,0 +1,117 @@
+import pytest
+
+from life import Universe
+from life import render
+
+class TestElementNextState:
+    test_state = [
+            #0,1,2,3,4,5,6,7,8,9
+            [1,0,0,1,0,0,0,0,0,0], #0
+            [0,0,0,1,0,0,0,1,0,0], #1
+            [1,0,0,1,0,0,0,1,0,0], #2
+            [1,1,0,0,0,0,0,1,0,0], #3
+            [1,0,1,1,0,0,0,0,0,0], #4
+            #0,1,2,3,4,5,6,7,8,9
+            ]
+
+    def setup(self):
+        universe = Universe(size=5)
+        universe.state = self.test_state
+        return universe
+
+    def test_input(self):
+        """To ensure that the input is correct for the tests."""
+        assert sum([sum(row) for row in self.test_state]) == 13
+
+    # Live cell next generation tests
+    def test_live_cell_with_fewer_than_two_live_neighbors_dies(self):
+        universe = self.setup()
+        # Live cell with 1 live neighbor
+        assert universe.element_next_state(0,3) == 0 
+        assert universe.element_next_state(2,3) == 0 
+        assert universe.element_next_state(1,7) == 0
+        assert universe.element_next_state(3,7) == 0
+
+        # Live cell with 0 live neighbors
+        assert universe.element_next_state(0,0) == 0
+
+    def test_live_cell_with_two_or_three_live_neighbors_lives(self):
+        universe = self.setup()
+        # Live cell with two live neighbors
+        assert universe.element_next_state(2,0) == 1 
+        assert universe.element_next_state(4,0) == 1
+
+        #Live cell with three live neighbors
+        assert universe.element_next_state(3,0) == 1 
+
+    def test_live_cell_with_more_than_three_live_neighbors_dies(self):
+        universe = self.setup()
+        assert universe.element_next_state(3,1) == 0
+        assert universe.element_next_state(4,3) == 0
+
+    # Dead cell next generation tests
+    def test_dead_cell_with_three_live_neighbors_becomes_alive(self):
+        universe = self.setup()
+        assert universe.element_next_state(1,2) == 1
+        assert universe.element_next_state(1,4) == 1
+        assert universe.element_next_state(2,6) == 1
+        assert universe.element_next_state(2,8) == 1
+
+    def test_dead_cell_with_fewer_than_three_live_neighbors_stays_dead(self):
+        universe = self.setup()
+        # Dead cell with 2 live neighbors
+        assert universe.element_next_state(1,0) == 0
+        assert universe.element_next_state(2,4) == 0
+
+        # Dead cell with 1 live neighbor
+        assert universe.element_next_state(0,1) == 0
+        assert universe.element_next_state(0,7) == 0
+
+        # Dead cell with 0 live neighbors
+        assert universe.element_next_state(0,9) == 0
+        assert universe.element_next_state(4,9) == 0
+
+    def test_dead_cell_with_more_than_three_live_neighbors_stays_dead(self):
+        universe = self.setup()
+        assert universe.element_next_state(4,1) == 0
+        assert universe.element_next_state(4,7) == 0
+
+class TestUniverseNextState:
+    def test_glider_next_state(self):
+        universe = Universe(size=5, configuration='glider')
+        universe.next()
+
+        expected_next_state = [
+                #0,1,2,3,4,5,6,7,8,9
+                [0,0,0,0,0,0,0,0,1,0], #0
+                [0,0,0,0,0,0,1,1,0,0], #1
+                [0,0,0,0,0,0,0,1,1,0], #2
+                [0,0,0,0,0,0,0,0,0,0], #3
+                [0,0,0,0,0,0,0,0,0,0], #4
+                #0,1,2,3,4,5,6,7,8,9
+                ]
+        assert universe.state == expected_next_state
+
+class TestRender:
+    # TODO: Got to figure out how to test this
+    @pytest.mark.xfail
+    def test_render_output(self, capfd):
+        universe = Universe(size=5, configuration='glider')
+        # Prints the universe to console
+        render(universe)
+
+        out, err = capfd.readouterr()
+        expected_out ="""
+        ------------
+        ı       ■  ı
+        ı       ■ ■ı
+        ı       ■■ ı
+        ı          ı
+        ı          ı
+        ------------
+        """.strip()
+        assert out[:19] == expected_out[:19]
+        # print(out[9:19])
+        # print(expected_out[9:19])
+
+        
